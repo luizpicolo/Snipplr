d13a8cf9a7b3730a4b24d91fc485356c87544a44
Luiz Picolo <luizpicolo@gmail.com>
2014-01-17 10:25:24 -0300

    init commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,79 @@
 $(document).ready(function(){
-    $('.snipplr').click(function(){
-        $( "#dialog" ).dialog({
-          autoOpen: false,
-          show: {
-            effect: "blind",
-            duration: 1000
-          },
-          hide: {
-            effect: "explode",
-            duration: 1000
-          }
+    $('.show-snipplr').click(function(){
+        
+        var title = $(this).children('h1').text();
+        var description = $(this).children('p').text();
+        var code = '&lt;table class="tabela"&gt;' +
+                   '...' + 
+                   '&lt;/table&gt;';
+        
+        var modal = '<div class="modal-dialog">'+
+                    '<div class="modal-content">' +
+                        '<div class="modal-header">' +
+                            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                            '<h1 class="modal-title">'+title+'</h1>' +
+                            '<p>'+description+'</p>' +
+                        '</div>' +
+                        '<div class="modal-body">' +
+                            '<pre class="prettyprint lang-scm">'+code+'</pre>' +
+                        '</div>' +
+                        '<div class="modal-footer">' +
+                            '<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>' +
+                        '</div>' +
+                    '</div>' +
+                   '</div>';
+        
+        
+        
+        $(".modal").html(modal).modal({
+          keyboard: false
         });
+    })
+    
+    $('#registering-snipplr').click(function(){
+        
+         var form = '<div class="modal-dialog">'+
+                    '<div class="modal-content">' +
+                        '<div class="modal-header">' +
+                            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                            '<h1 class="modal-title">Cadastrar Snipplr</h1>' +
+                            '<p>asdfas</p>' +
+                        '</div>' +
+                        '<div class="modal-body">' +
+                            '<form role="form">' +
+                              '<div class="form-group">' +
+                                '<label for="titulo">Titulo</label>' +
+                                '<input type="text" class="form-control" name="titulo" id="titulo" placeholder="Titulo">' +
+                              '</div>' +
+                              '<div class="form-group">' +
+                                '<label for="descricao">Descrição</label>' +
+                                '<input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">' +
+                              '</div>' +
+                              '<div class="form-group">' +
+                                '<label for="categoria">Categoria</label>' +
+                                '<select id="categoria" name="categoria" class="form-control">' +
+                                  '<option>1</option>' +
+                                  '<option>2</option>' +
+                                  '<option>3</option>' +
+                                  '<option>4</option>' +
+                                  '<option>5</option>' +
+                                '</select>' +
+                              '</div>' +
+                              '<div class="form-group">' +
+                                '<label for="codigo">Código</label>' +
+                                '<textarea class="form-control"  id="codigo" name="codigo" rows="3"></textarea>' +
+                              '</div>' +
+                            '</form>' +
+                        '</div>' +
+                        '<div class="modal-footer">' +
+                            '<button type="submit" class="btn btn-primary">Cadastrar</button>' +
+                            '<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>' +
+                        '</div>' +
+                    '</div>' +
+                   '</div>';
+        
+        $(".modal").html(form).modal({
+          keyboard: false
+        });    
     })
 })
--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Indie+Flower' rel='stylesheet' type='text/css'>
     
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
     
-    <script src="assets/js/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     <script src="assets/js/main.js"></script>
 </head>
@@ -18,53 +18,56 @@
         <h1>Snipplr</h1>
     </header>   
     <main>
+        <div class="text-right">
+            <button type="button" class="btn btn-primary" id="registering-snipplr">Cadastrar Snipplr</button>
+        </div>
         <div class="row">
             <div class="col-md-3 code-java">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr" data-id="1">
+                    <h1><i class="fa fa-code"></i> Código Java</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             <div class="col-md-3 code-php">
-                <section class="snipplr">
+                <section class="show-snipplr">
                     <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             <div class="col-md-3 code-python">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr">
+                    <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             <div class="col-md-3 code-js">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr">
+                    <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             
             <div class="col-md-3 code-js">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr">
+                    <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             <div class="col-md-3 code-python">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr">
+                    <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             <div class="col-md-3 code-php">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr">
+                    <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
             <div class="col-md-3 code-java">
-                <section class="snipplr">
-                    <h1>Código</h1>
+                <section class="show-snipplr">
+                    <h1><i class="fa fa-code"></i> Código</h1>
                     <p>Descrição</p>
                 </section>
             </div>
@@ -73,6 +76,7 @@
     <footer>
         
     </footer>
-    <div id="modal"></div>
+    <div class="modal"></div>
+    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
 </body>
 </html>
