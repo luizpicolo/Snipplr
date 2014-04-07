ffc91d62a06863988c3a7752727f55dc44c3880e
Luiz Picolo <luizpicolo@gmail.com>
2014-04-07 16:43:02 -0400

    Ajustes na identação do JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,12 @@ var snipplr = function(string){
             var html = "";
             for (var i = 0; i < objs.length; i+=1){
                 html += '<div class="col-md-3 code-'+objs[i].categoria+'">' +
-                            '<section class="show-snipplr">' +
-                                '<h1>'+objs[i].titulo+'</h1>' +
-                                '<p>'+objs[i].descricao+'</p>' +
-                                '<code>'+objs[i].codigo+'</code>' +
-                            '</section>' +
-                        '</div>';    
+                '<section class="show-snipplr">' +
+                '<h1>'+objs[i].titulo+'</h1>' +
+                '<p>'+objs[i].descricao+'</p>' +
+                '<code>'+objs[i].codigo+'</code>' +
+                '</section>' +
+                '</div>';    
             }
             $('.row').html(html);
         } else {
@@ -30,36 +30,36 @@ var alertMSG = function(string, type){
     
     switch (type){
         case 1 :
-            $('.alertMSG').html(
-                '<div class="alert alert-success">'+string+'</div>'
+        $('.alertMSG').html(
+            '<div class="alert alert-success">'+string+'</div>'
             );
-            setInterval(function(){
-                $('.alert').fadeOut('slow');    
-            },timeClose);
-            break;
+        setInterval(function(){
+            $('.alert').fadeOut('slow');    
+        },timeClose);
+        break;
         case 2 :
-            $('.alertMSG').html(
-                '<div class="alert alert-info">'+string+'</div>'
+        $('.alertMSG').html(
+            '<div class="alert alert-info">'+string+'</div>'
             );
-            setInterval(function(){
-                $('.alert').fadeOut('slow');    
-            }, timeClose);
-            break;
+        setInterval(function(){
+            $('.alert').fadeOut('slow');    
+        }, timeClose);
+        break;
         case 3 :
-            $('.alertMSG').html(
-                '<div class="alert alert-warning">'+string+'</div>'
+        $('.alertMSG').html(
+            '<div class="alert alert-warning">'+string+'</div>'
             );
-            setInterval(function(){
-                $('.alert').fadeOut('slow');    
-            }, timeClose);
-            break;
+        setInterval(function(){
+            $('.alert').fadeOut('slow');    
+        }, timeClose);
+        break;
         case 4 :
-            $('.alertMSG').html(
-                '<div class="alert alert-danger">'+string+'</div>'
+        $('.alertMSG').html(
+            '<div class="alert alert-danger">'+string+'</div>'
             );
-            setInterval(function(){
-                $('.alert').fadeOut('slow');    
-            }, timeClose);
+        setInterval(function(){
+            $('.alert').fadeOut('slow');    
+        }, timeClose);
         break;
     }    
 }
@@ -80,71 +80,71 @@ $(document).ready(function(){
         var title = $(this).children('h1').text();
         var description = $(this).children('p').text();
         var code = $(this).children('code').text();
-                
+        
         var view = '<div class="modal-dialog">'+
-                    '<div class="modal-content">' +
-                        '<div class="modal-header">' +
-                            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                            '<h1 class="modal-title">'+title+'</h1>' +
-                            '<p>'+description+'</p>' +
-                        '</div>' +
-                        '<div class="modal-body">' +
-                            '<pre>'+code+'</pre>' +
-                        '</div>' +
-                        '<div class="modal-footer">' +
-                            '<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>' +
-                        '</div>' +
-                    '</div>' +
-                   '</div>';
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+        '<h1 class="modal-title">'+title+'</h1>' +
+        '<p>'+description+'</p>' +
+        '</div>' +
+        '<div class="modal-body">' +
+        '<pre>'+code+'</pre>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
 
         // Incorpora o html das visualizações do codigo no modal
         $(".modal").html(view).fadeIn('3000').modal({keyboard: false});
     })
-    
+
     // Carrega o formulário para novos cadastros
     $('#registering-snipplr').click(function(){
-            
-         var form = '<div class="modal-dialog">'+
-                    '<div class="modal-content">' +
-                        '<div class="modal-header">' +
-                            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                            '<h1 class="modal-title">Cadastrar Snipplr</h1>' +
-                        '</div>' +
-                        '<div class="modal-body">' +
-                            '<form method="post" id="code-snipplr" action="">' +
-                              '<div class="form-group">' +
-                                '<label for="titulo">Titulo</label>' +
-                                '<input type="text" class="form-control" name="titulo" id="titulo" placeholder="Titulo" required>' +
-                              '</div>' +
-                              '<div class="form-group">' +
-                                '<label for="descricao">Descrição</label>' +
-                                '<input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">' +
-                              '</div>' +
-                              '<div class="form-group">' +
-                                '<label for="categoria">Categoria</label>' +
-                                '<select id="categoria" name="categoria" class="form-control">' +
-                                  '<option value="1">PHP</option>' +
-                                  '<option value="2">Java</option>' +
-                                  '<option value="3">Ruby</option>' +
-                                  '<option value="4">JavaScript</option>' +
-                                '</select>' +
-                              '</div>' +
-                              '<div class="form-group">' +
-                                '<label for="codigo">Código</label>' +
-                                '<textarea class="form-control"  id="codigo" name="codigo" rows="3"></textarea>' +
-                              '</div>' +
-                              '<input type="submit" class="btn btn-primary" value="Cadastrar">' +
-                            '</form>' +
-                        '</div>' +
-                    '</div>' +
-                   '</div>';
         
+       var form = '<div class="modal-dialog">'+
+       '<div class="modal-content">' +
+       '<div class="modal-header">' +
+       '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+       '<h1 class="modal-title">Cadastrar Snipplr</h1>' +
+       '</div>' +
+       '<div class="modal-body">' +
+       '<form method="post" id="code-snipplr" action="">' +
+       '<div class="form-group">' +
+       '<label for="titulo">Titulo</label>' +
+       '<input type="text" class="form-control" name="titulo" id="titulo" placeholder="Titulo" required>' +
+       '</div>' +
+       '<div class="form-group">' +
+       '<label for="descricao">Descrição</label>' +
+       '<input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">' +
+       '</div>' +
+       '<div class="form-group">' +
+       '<label for="categoria">Categoria</label>' +
+       '<select id="categoria" name="categoria" class="form-control">' +
+       '<option value="1">PHP</option>' +
+       '<option value="2">Java</option>' +
+       '<option value="3">Ruby</option>' +
+       '<option value="4">JavaScript</option>' +
+       '</select>' +
+       '</div>' +
+       '<div class="form-group">' +
+       '<label for="codigo">Código</label>' +
+       '<textarea class="form-control"  id="codigo" name="codigo" rows="3"></textarea>' +
+       '</div>' +
+       '<input type="submit" class="btn btn-primary" value="Cadastrar">' +
+       '</form>' +
+       '</div>' +
+       '</div>' +
+       '</div>';
+       
         // Incorpora o form no modal
         $(".modal").html(form).fadeIn('3000').modal({keyboard: false});    
     })
+
+$('#clear-snipplr').click(function(){
     
-    $('#clear-snipplr').click(function(){
-        
         // Verifica se há dados no localStorage a serem deletados
         // Se verdadeiro limpa, caso não, mostra um mensagem informativa
         if (localStorage.getItem('snipplr')){
@@ -159,7 +159,7 @@ $(document).ready(function(){
         // Carrega os dados e monta a apresentação dos Snnipers
         snipplr(localStorage.getItem('snipplr'));
     })
-      
+
     // Executa ao submiter o formulario
     $("form").live("submit", function(e) {
         e.preventDefault();  
